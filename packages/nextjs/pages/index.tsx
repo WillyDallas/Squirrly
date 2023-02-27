08c624b3623fa2246e9f39c65bf6970a55cc3b29
willydallas <willy.dallas@pm.me>
2023-02-27 10:02:19 -0700

    first commit for ETHDenver ALL NEW CODE

---
+++ packages/nextjs/pages/index.tsx
@@ -0,0 +1,33 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { BugAntIcon, SparklesIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import React, { use } from "react";
+import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
+
+
+
+const Home: NextPage = () => {
+
+  const { writeAsync: doCheckin } = useScaffoldContractWrite("YourContract", "checkin", null, "0.001");
+
+  return (
+    <>
+      <Head>
+        <title>Scaffold-eth App</title>
+        <meta name="description" content="Created with 🏗 scaffold-eth" />
+      </Head>
+
+      <div className="flex items-center flex-col flex-grow pt-10">
+        <button className="btn btn-primary"
+        onClick={()=>{
+          doCheckin();
+        }}>Check In</button>
+      
+      </div>
+        
+    </>
+  );
+};
+
+export default Home;
