7738c3c3618d34b31cf23ac699770e3f1b23b225
resjudicoda <adam.j.reis@gmail.com>
2023-02-28 09:32:11 -0700

    basic screen size scroll snap

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -7,6 +7,31 @@ import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 
 
+// const Home: NextPage = () => {
+
+//   const { writeAsync: doCheckin } = useScaffoldContractWrite("YourContract", "checkin", null, "0.001");
+
+//   return (
+//     <>
+//       <Head>
+//         <title>Scaffold-eth App</title>
+//         <meta name="description" content="Created with 🏗 scaffold-eth" />
+//       </Head>
+
+//       <div className="flex items-center flex-col flex-grow pt-10">
+//         <button className="btn btn-primary"
+//         onClick={()=>{
+//           doCheckin();
+//         }}>Check In</button>
+      
+//       </div>
+        
+//     </>
+//   );
+// };
+
+// export default Home;
+
 const Home: NextPage = () => {
 
   const { writeAsync: doCheckin } = useScaffoldContractWrite("YourContract", "checkin", null, "0.001");
@@ -18,13 +43,12 @@ const Home: NextPage = () => {
         <meta name="description" content="Created with 🏗 scaffold-eth" />
       </Head>
 
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <button className="btn btn-primary"
-        onClick={()=>{
-          doCheckin();
-        }}>Check In</button>
-      
-      </div>
+      <main className="max-h-screen overflow-y-scroll snap snap-y snap-mandatory">
+        <section className="w-full h-screen bg-sky-200 snap-start">Sky</section>
+        <section className="w-full h-screen bg-green-200 snap-start">Canopy</section>
+        <section className="w-full h-screen bg-amber-400 snap-start">Trees</section>
+        <section className="w-full h-screen bg-yellow-200 snap-start">Forrest Floor</section>
+      </main>
         
     </>
   );
