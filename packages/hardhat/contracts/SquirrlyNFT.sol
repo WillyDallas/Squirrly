80999430845a80dad69ca0d3917794815a954133
willydallas <willy.dallas@pm.me>
2023-02-27 17:19:33 -0700

    erc721 tinkering

---
+++ packages/hardhat/contracts/SquirrlyNFT.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// Useful for debugging. Remove when deploying to a live network.
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract SquirrlyNFT is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor(address _owner) ERC721("Squirrly", "SQRL") {}
+
+    function safeMint(address to) public {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+}
