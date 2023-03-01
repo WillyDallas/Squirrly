7002ef3900aecf2ab1b7fac5033a1f39a9478518
resjudicoda <adam.j.reis@gmail.com>
2023-02-28 21:13:37 -0700

    starting functionality

--- packages/nextjs/components/ScrollUi/Quiz.tsx
+++ packages/nextjs/components/ScrollUi/Quiz.tsx
@@ -2,11 +2,26 @@ import { useEffect, useRef, useState } from "react";
 import questions from "./questions.json";
 import { ForwardIcon, BackwardIcon } from "@heroicons/react/24/outline";
 
+interface Effect {
+  econ: number;
+  dipl: number;
+  govt: number;
+  scty: number;
+}
+
 export default function Quiz() {
   const [loading, setLoading] = useState(false);
   //const [questions, setQuestions] = useState([]);
   const [step, setStep] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const multiplier = {
+    "Strongly Agree": 1.0,
+    "Agree": 0.5,
+    "Neutral": 0.0,
+    "Disagree": -0.5,
+    "Strongly Disagree": -1.0
+  }
 
   const handlePrevious = () => {
     const prevQues = currentQuestion - 1;
@@ -17,15 +32,29 @@ export default function Quiz() {
     nextQues < questions.length && setCurrentQuestion(nextQues);
   };
 
+  const handleSubmit = (answer: string, ) => {
+
+  }
+
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <div className="flex flex-col items-center justify-between rounded-lg border border-sky-700 w-9/12 md:h-96 h-96">
-        <div>
-          <div className="mt-4 text-xl text-black">{questions[currentQuestion].question}</div>
-          <div className="mt-4 text-xl text-black">
+      <div className="flex flex-col items-center justify-between rounded-lg border border-sky-700 w-9/12 md:h-[36rem] h-[36rem]">
+        <div className="flex flex-col items-center">
+          <div className="mt-4 text-xl text-black text-center">{questions[currentQuestion].question}</div>
+          <div className="mt-4 text-xl text-black flex flex-col items-center">
             {questions[currentQuestion].answerOptions &&
               questions[currentQuestion].answerOptions.map((el, idx) => {
-                return <p key={idx}>{el.answer}</p>;
+                return (
+                  <>
+                    <button
+                      className="btn btn-primary rounded-full capitalize font-normal font-white flex items-center gap-1 hover:gap-2 transition-all tracking-wide"
+                      key={idx}
+                    >
+                      {el.answer}
+                    </button>
+                    <br></br>
+                  </>
+                );
               })}
           </div>
         </div>
@@ -44,3 +73,6 @@ export default function Quiz() {
     </div>
   );
 }
+
+// set type on questions. if it is type scale, hard code buttons
+// if it is type fixed, loop through answer options
