aeace548e3a1d711be326259010732ff3ba019f6
resjudicoda <adam.j.reis@gmail.com>
2023-02-28 20:05:41 -0700

    form centered and stepping

--- packages/nextjs/components/ScrollUi/Quiz.tsx
+++ packages/nextjs/components/ScrollUi/Quiz.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import questions from "./questions.json";
+import { ForwardIcon, BackwardIcon } from "@heroicons/react/24/outline";
 
 export default function Quiz() {
   const [loading, setLoading] = useState(false);
@@ -17,26 +18,29 @@ export default function Quiz() {
   };
 
   return (
-    <div className="flex flex-col justify-center h-full">
-      <div className="flex flex-col items-center w-full rounded-lg border border-sky-700 ht-96">
-        <div className="mt-4 text-2xl text-black">{questions[currentQuestion].question}</div>
-        <div className="mt-4 text-2xl text-black">
-            {questions[currentQuestion].answerOptions && questions[currentQuestion].answerOptions.map((el, idx) => {
-              return <p key={idx}>{el.answer}</p>;
-            })}
-        </div>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="flex flex-col items-center justify-between rounded-lg border border-sky-700 w-9/12 md:h-96 h-96">
         <div>
-          <button onClick={handlePrevious} className="w-[49%] py-3 bg-indigo-600 rounded-lg">
-            Previous
+          <div className="mt-4 text-xl text-black">{questions[currentQuestion].question}</div>
+          <div className="mt-4 text-xl text-black">
+            {questions[currentQuestion].answerOptions &&
+              questions[currentQuestion].answerOptions.map((el, idx) => {
+                return <p key={idx}>{el.answer}</p>;
+              })}
+          </div>
+        </div>
+        <div className="w-6/12 flex flex-row justify-evenly">
+          <button onClick={handlePrevious} className="w-12 py-3 bg-indigo-600 rounded-lg flex flex-row justify-center">
+            <BackwardIcon className="h-8 w-8" />
           </button>
-          <button onClick={handleNext} className="w-[49%] py-3 bg-indigo-600 rounded-lg">
-            Next
+          <button onClick={handleNext} className="w-12 py-3 bg-indigo-600 rounded-lg flex flex-row justify-center">
+            <ForwardIcon className="h-8 w-8" />
           </button>
         </div>
-        <h4 className="mt-10 text-xl text-black/60">
-          Question {currentQuestion + 1} of {questions.length}
-        </h4>
       </div>
+      <h4 className="mt-10 text-xl text-black/60">
+        Question {currentQuestion + 1} of {questions.length}
+      </h4>
     </div>
   );
 }
