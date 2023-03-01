516f4501e856dd532816fe4fa352e2637caefc74
willydallas <willy.dallas@pm.me>
2023-03-01 09:12:06 -0700

    small update

--- packages/hardhat/contracts/SquirrlyNFT.sol
+++ packages/hardhat/contracts/SquirrlyNFT.sol
@@ -7,18 +7,39 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract SquirrlyNFT is ERC721, Ownable {
+contract SquirrlyNFT is ERC721URIStorage, Pausable, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
     constructor() ERC721("Squirrly", "SQRL") {}
 
-    function safeMint(address to) public payable{
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to, uint256 tokenId, string memory uri) public, payable{
         _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 
     receive() external payable {
