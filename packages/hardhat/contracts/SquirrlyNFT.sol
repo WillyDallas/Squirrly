0eb86ca264a1cbb7a686f18d20e10cd55d124dfb
willydallas <willy.dallas@pm.me>
2023-03-01 08:57:45 -0700

    asset store and basic uri storage implemented

--- packages/hardhat/contracts/SquirrlyNFT.sol
+++ packages/hardhat/contracts/SquirrlyNFT.sol
@@ -1,18 +1,19 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.19;
 
 // Useful for debugging. Remove when deploying to a live network.
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SquirrlyNFT is ERC721, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor(address _owner) ERC721("Squirrly", "SQRL") {}
+    constructor() ERC721("Squirrly", "SQRL") {}
 
     function safeMint(address to) public payable{
         uint256 tokenId = _tokenIdCounter.current();
