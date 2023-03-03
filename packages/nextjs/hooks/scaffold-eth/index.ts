f8e7aad7e875d2da5dcc2a07d15b712a61d3afb7
resjudicoda <adam.j.reis@gmail.com>
2023-03-02 17:32:27 -0700

    checking balance

--- packages/nextjs/hooks/scaffold-eth/index.ts
+++ packages/nextjs/hooks/scaffold-eth/index.ts
@@ -8,3 +8,6 @@ export * from "./useScaffoldContractWrite";
 export * from "./useDeployedContractInfo";
 export * from "./useScaffoldEventSubscriber";
 export * from "./useNetworkColor";
+export * from "./useAccountBalance";
+export * from "./useAnimationConfig";
+export * from "./useDeployedContractNames";
--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -3,13 +3,34 @@ import Head from "next/head";
 import { BugAntIcon, SparklesIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import React, { use } from "react";
-import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
-
-
+import { useScaffoldContractWrite, useDeployedContractInfo, useScaffoldContractRead } from "~~/hooks/scaffold-eth";
+import { useEffectOnce, useLocalStorage } from "usehooks-ts";
+import { useAutoConnect, useAccountBalance } from "~~/hooks/scaffold-eth";
+import { useAccount, useContractRead } from "wagmi";
+import { BigNumber } from "ethers";
 
 const Home: NextPage = () => {
+  //const { writeAsync: doCheckin } = useScaffoldContractWrite("SquirrlyNFT", "checkin", null, "0.001");
+  const { data: deployedContractData } = useDeployedContractInfo("SquirrlyNFT");
+  //console.log("deployed contract address", deployedContractData?.address);
+  const { address: accountAddress } = useAccount();
+  //console.log("account address", accountAddress);
 
-  const { writeAsync: doCheckin } = useScaffoldContractWrite("SquirrlyNFT", "checkin", null, "0.001");
+  //const { balance } = useAccountBalance(accountAddress);
+  //console.log('balance', balance)
+
+  //const balanceOf = useScaffoldContractRead<BigNumber>("SquirrlyNFT", "balanceOf", ['0x8555eB2a135074B67e95C5bA9297b0229e90B5A3']);
+  //const { data: balanceOf } = useContractRead(deployedContractData?.address, 'balanceOf', accountAddress)
+
+  const balanceRead = useContractRead({
+    address: deployedContractData?.address,
+    abi: deployedContractData?.abi,
+    functionName: 'balanceOf',
+    args: [accountAddress],
+  })
+  const { data } = balanceRead
+  const balance = data ? data.toNumber() : null
+  console.log("balance", balance);
 
   return (
     <>
@@ -19,13 +40,26 @@ const Home: NextPage = () => {
       </Head>
 
       <div className="flex items-center flex-col flex-grow pt-10">
-        <button className="btn btn-primary"
-        onClick={()=>{
-          doCheckin();
-        }}>Check In</button>
-      
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            doCheckin();
+          }}
+        >
+          Check In
+        </button>
       </div>
-        
+
+      <div className="flex items-center flex-col flex-grow pt-10">
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            checkIsCollectionOwner(accountAddress, deployedContractData?.address);
+          }}
+        >
+          Check Ownership
+        </button>
+      </div>
     </>
   );
 };
