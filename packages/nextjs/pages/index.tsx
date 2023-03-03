6e9d1f1e0a3778a2decead96a1cd1eb7b65c78a3
willydallas <willy.dallas@pm.me>
2023-03-03 11:34:02 -0700

    added push to IPFS functionality

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -4,25 +4,31 @@ import { BugAntIcon, SparklesIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import React, { use } from "react";
 import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
+import { storeNFT } from "../utils/pushToIpfs.js";
 
-
+const API_KEY = process.env.NEXT_PUBLIC_NFT_STORAGE_API_KEY;
 
 const Home: NextPage = () => {
-
-  const { writeAsync: doCheckin } = useScaffoldContractWrite("SquirrlyNFT", "checkin", null, "0.001");
-
   return (
     <>
       <Head>
         <title>Squirrly App</title>
-        <meta name="description" content="Created with 🏗 scaffold-eth" />
       </Head>
 
       <div className="flex items-center flex-col flex-grow pt-10">
-        <button className="btn btn-primary"
-        onClick={()=>{
-          doCheckin();
-        }}>Check In</button>
+      <button
+        className="btn btn-primary"
+        onClick={async () => {
+          const econ = 10;
+          const dipl = 20;
+          const govt = 30;
+          const scty = 40;
+          const powers = ["super jump", "invisibility"];
+          await storeNFT(econ, dipl, govt, scty, powers, API_KEY);
+        }}
+      >
+        Store NFT
+      </button>
       
       </div>
         
