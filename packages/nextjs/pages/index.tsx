bebfed84851e6e3f1fbb24f027091aac1c24f97f
willydallas <willy.dallas@pm.me>
2023-03-03 11:51:12 -0700

    fixed index.tsx

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -13,29 +13,10 @@ import { storeNFT } from "../utils/pushToIpfs.js";
 const API_KEY = process.env.NEXT_PUBLIC_NFT_STORAGE_API_KEY;
 
 const Home: NextPage = () => {
-  //const { writeAsync: doCheckin } = useScaffoldContractWrite("SquirrlyNFT", "checkin", null, "0.001");
-  const { data: deployedContractData } = useDeployedContractInfo("SquirrlyNFT");
-  //console.log("deployed contract address", deployedContractData?.address);
   const { address: accountAddress } = useAccount();
-  //console.log("account address", accountAddress);
-
-  //const { balance } = useAccountBalance(accountAddress);
-  //console.log('balance', balance)
-
   const { data: balanceOf } = useScaffoldContractRead<BigNumber>("SquirrlyNFT", "balanceOf", {
     args: [accountAddress],
   });
-  console.log("balanceOf", balanceOf);
-  console.log("number", balanceOf?.toNumber());
-  // const balanceRead = useContractRead({
-  //   address: deployedContractData?.address,
-  //   abi: deployedContractData?.abi,
-  //   functionName: 'balanceOf',
-  //   args: [accountAddress],
-  // })
-  // const { data } = balanceRead
-  // const balance = data ? data.toNumber() : null
-  // console.log("balance", balance);
 
   const callCheckOwnershipAPI = async (param: any) => {
     try {
@@ -60,13 +41,18 @@ const Home: NextPage = () => {
 
       <div className="flex items-center flex-col flex-grow pt-10">
         <button
-          className="btn btn-primary"
-          onClick={() => {
-            doCheckin();
-          }}
-        >
-          Check In
-        </button>
+        className="btn btn-primary"
+        onClick={async () => {
+          const econ = 10;
+          const dipl = 20;
+          const govt = 30;
+          const scty = 40;
+          const powers = ["super jump", "invisibility"];
+          await storeNFT(econ, dipl, govt, scty, powers, API_KEY);
+        }}
+      >
+        Store NFT
+      </button>
       </div>
     </>
   );
